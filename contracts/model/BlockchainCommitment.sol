24096b3f88f74259cc678c5d2f4ac6434cc4d9db
Marcello Bertolini <marcello.bertolini@mail.polimi.it>
2020-04-30 11:35:03 +0200

    signCommitment implemented

--- contracts/model/BlockchainCommitment.sol
+++ contracts/model/BlockchainCommitment.sol
@@ -6,6 +6,10 @@ abstract contract BlockchainCommitment is Commitment {
     // store actors accounts
     address public debtor;
     address public creditor;
+
+    bool private creditorApproved;
+    bool private debtorApproved;
+
     enum ControlFlowStatus {OK, Warning}
     ControlFlowStatus private warning;
 
@@ -14,8 +18,8 @@ abstract contract BlockchainCommitment is Commitment {
     mapping(string => address) private documentOwners;
 
 
-    enum DocumentType {START, END, SCOPE}
-    mapping(string => DocumentType) private documentTypes;
+    enum DocumentCategory {INIT_SCOPE, TERMINATE_SCOPE, SCOPE}
+    mapping(string => DocumentCategory) private documentCategories;
 
 
     bool private inCommitmentWin;
@@ -39,8 +43,13 @@ abstract contract BlockchainCommitment is Commitment {
         
     }
 
-    modifier onlyParticipants (address _documentOwner){
-        require(_documentOwner != address(0) && (_documentOwner == creditor || _documentOwner == debtor), "Document owner not valid");
+    modifier onlyParticipants (address _participant){
+        require(_participant != address(0) && (_participant == creditor || _participant == debtor), "participant not valid");
+        _;
+    }
+
+    modifier commitmentSigned() {
+        require(creditorApproved && debtorApproved, "the commitment has not been signed yet");
         _;
     }
 
@@ -62,16 +71,25 @@ abstract contract BlockchainCommitment is Commitment {
     }
 
     /*
-        creates a new document and sets its type.
+        creates a new document and sets its cateogory.
         `_documentId`
         `_documentOwner` the owner of the document. Only actor that can upload the document in the future
-        `_documentType` can be "start", "scope", "end"
+        `_documentCategory` can be "InitScope", "Scope", "TerminateScope"
     */
-    function initDocument(string memory _documentId, address _documentOwner, string memory _documentType) public onlyOwner onlyNull onlyParticipants(_documentOwner) {
-        require(checkDocumentType(_documentType), "Document type not valid");
+    function initDocument(string memory _documentId, address _documentOwner, string memory _documentCategory) public onlyOwner onlyNull onlyParticipants(_documentOwner) {
+        require(checkDocumentCategory(_documentCategory), "Document category not valid");
         documentOwners[_documentId] = _documentOwner;
-        documentTypes[_documentId] = getDocumentType(_documentType);
+        documentCategories[_documentId] = getDocumentCategory(_documentCategory);
         onInitDocument(_documentId, _documentOwner);
+    }
+
+    function signCommitment() public onlyParticipants(msg.sender) onlyNull{
+        if (msg.sender == creditor){
+            creditorApproved = true;
+        }
+        else if (msg.sender == debtor){
+            debtorApproved = true;
+        }
     }
 
     /* 
@@ -80,22 +98,22 @@ abstract contract BlockchainCommitment is Commitment {
         `_documentId` the document to post
         `_documentData` the document payload
     */ 
-    function postDocument(string memory _documentId, uint _documentData) public {
+    function postDocument(string memory _documentId, uint _documentData) public commitmentSigned {
         require(msg.sender == documentOwners[_documentId], "Document owner not valid");
-        if(!inCommitmentWin && documentTypes[_documentId] == DocumentType.START){
+        if(!inCommitmentWin && documentCategories[_documentId] == DocumentCategory.INIT_SCOPE){
             inCommitmentWin = true;
             onDocumentPosted(_documentId, _documentData, false);
             onTargetStarts();
 
         }
-        else if(inCommitmentWin && documentTypes[_documentId] == DocumentType.END){
+        else if(inCommitmentWin && documentCategories[_documentId] == DocumentCategory.TERMINATE_SCOPE){
             inCommitmentWin = false;
             afterCommitmentWin = true;
             onDocumentPosted(_documentId, _documentData, false);
             onTargetEnds();
 
         }
-        else if(inCommitmentWin && documentTypes[_documentId] == DocumentType.SCOPE){
+        else if(inCommitmentWin && documentCategories[_documentId] == DocumentCategory.SCOPE){
             onDocumentPosted(_documentId, _documentData, false);
         }
         else {
@@ -123,33 +141,33 @@ abstract contract BlockchainCommitment is Commitment {
         return warning == ControlFlowStatus.Warning;
     }
 
-    function getDocumentType(string memory _documentType) private pure returns(DocumentType){
+    function getDocumentCategory(string memory _documentType) private pure returns(DocumentCategory){
         bytes memory documentType = bytes(_documentType);
         bytes32 documentHash = keccak256(documentType);
 
-        if(documentHash == keccak256("start") || documentHash == keccak256("Start")){
-            return DocumentType.START;
+        if(documentHash == keccak256("InitScope")){
+            return DocumentCategory.INIT_SCOPE;
         }
-        if(documentHash == keccak256("scope") || documentHash == keccak256("Scope")){
-            return DocumentType.SCOPE;
+        if(documentHash == keccak256("Scope")){
+            return DocumentCategory.SCOPE;
         }
-        if(documentHash == keccak256("end") || documentHash == keccak256("End")){
-            return DocumentType.END;
+        if(documentHash == keccak256("TerminateScope")){
+            return DocumentCategory.TERMINATE_SCOPE;
         }
 
     }
 
-    function checkDocumentType(string memory _documentType) private pure returns(bool){
+    function checkDocumentCategory(string memory _documentType) private pure returns(bool){
         bytes memory documentType = bytes(_documentType);
         bytes32 documentHash = keccak256(documentType);
 
-        if(documentHash == keccak256("start") || documentHash == keccak256("Start")){
+        if(documentHash == keccak256("InitScope")){
             return true;
         }
-        else if(documentHash == keccak256("scope") || documentHash == keccak256("Scope")){
+        else if(documentHash == keccak256("Scope")){
             return true;
         }
-        else if(documentHash == keccak256("end") || documentHash == keccak256("End")){
+        else if(documentHash == keccak256("TerminateScope")){
             return true;
         }
         else {
